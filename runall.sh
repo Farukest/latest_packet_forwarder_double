67791d0c2038f6e27247aef85a3f6cabaf52797f
Farukest <abdullahfarukozden@gmail.com>
2023-12-30 23:05:56 +0300

    initial

--- runall.sh
+++ runall.sh
@@ -5,7 +5,9 @@ if [[ $count -eq 2 ]];
 then 
 	echo "ALREADY RUNNING";
 else
-	pgrep lora_pkt_fwd$i | xargs kill		
+	pgrep lora_pkt_fwd$i | xargs kill
+	sleep 5	
 	cd /home/latest_packet_forwarder/sx1302_hal/packet_forwarder/ && ./lora_pkt_fwd &
+	sleep 5
 	cd /home/latest_packet_forwarder2/sx1302_hal/packet_forwarder/ && ./lora_pkt_fwd &    
 fi       
