150a56ff3762d5b1af87dac16837e5b3758a26a1
Farukest <abdullahfarukozden@gmail.com>
2023-12-30 23:32:39 +0300

    initial

--- runall.sh
+++ runall.sh
@@ -7,7 +7,7 @@ then
 else
 	pgrep lora_pkt_fwd$i | xargs kill
 	sleep 5	
-	cd /home/latest_packet_forwarder/sx1302_hal/packet_forwarder/ && ./lora_pkt_fwd &
+	cd /home/latest_packet_forwarder/sx1302_hal/packet_forwarder/ && ./lora_pkt_fwd -c /home/latest_packet_forwarder/hs_ft_pf_conf.json &
 	sleep 5
-	cd /home/latest_packet_forwarder2/sx1302_hal/packet_forwarder/ && ./lora_pkt_fwd &    
+	cd /home/latest_packet_forwarder2/sx1302_hal/packet_forwarder/ && ./lora_pkt_fwd -c /home/latest_packet_forwarder2/hs_ft_pf_conf.json &    
 fi       
